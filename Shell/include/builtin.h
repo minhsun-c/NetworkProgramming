197b935c7336a2f60d33d95c6e6440678da02e8f
chmh0624 <harrychang2003@gmail.com>
2024-10-10 16:19:40 +0800

    To difficult to maintain

--- Shell/include/builtin.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BUILTIN_COMMAND_H
-#define __BUILTIN_COMMAND_H
-
-#include "includes.h"
-
-void builtin(command_t *cmd);
-
-#endif
--- Shell/include/hash.h
+++
@@ -1,6 +0,0 @@
-#ifndef __HASH_H
-#define __HASH_H
-
-unsigned int hash(const char *str);
-
-#endif
