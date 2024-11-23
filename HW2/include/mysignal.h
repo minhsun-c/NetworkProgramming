57980640eebc8b5235996a3e078ef6908fb03b0a
minhsun-c <107985606+minhsun-c@users.noreply.github.com>
2024-11-23 16:05:21 +0800

    Remove error version

--- HW2/include/mysignal.h
+++
@@ -1,6 +0,0 @@
-#define SIG_MYTELL SIGUSR1
-#define SIG_MYYELL SIGUSR2
-
-void signal_tell_ctrl();
-void signal_yell_ctrl();
-void signal_child_ctrl();
--- HW2/include/server.h
+++
@@ -1,8 +0,0 @@
-void monitor_client(int pid, struct sockaddr_in *cliaddr);
-
-int connect_client(int connfd);
-
-void reset_server_socket(int *ssocket, struct sockaddr_in *serv_addr,
-                         char *serv_ip, int serv_port);
-
-void set_ip_port(int argc, char **argv, char *ip, int *port);
